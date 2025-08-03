aa5012d6589081127ac29c2697014123c81ecc57
Azure <shen46806@sas.edu.sg>
2025-08-03 10:09:03 +0800

    fix stainless steel jar lid dupe

--- overrides/kubejs/server_scripts/src/recipes/firmalife.js
+++ overrides/kubejs/server_scripts/src/recipes/firmalife.js
@@ -8,4 +8,9 @@ ServerEvents.recipes((e) => {
     })
 
     e.replaceInput({id: 'firmalife:crafting/climate_station'}, 'redstone', 'tfc:metal/sheet/wrought_iron')
+
+    e.remove({ input: 'lithicaddon:stainless_steel_lid', type: 'tfc:heating' });
+    e.recipes.tfc
+        .heating('lithicaddon:stainless_steel_lid', 1540)
+        .resultFluid(Fluid.of(`firmalife:metal/stainless_steel`, 4));
 })
